7ff9a690863f86e40f5046f45b35570d9a6d7126
mijnportfolio <68969664+mijnportfolio@users.noreply.github.com>
2020-08-15 12:12:38 +0200

    Nav bar

--- js/script.js
+++ js/script.js
@@ -25,10 +25,10 @@ $(document).ready(function(){
 	      //.log to determine when you want the 
 	      //nav bar to stick.  
 	      console.log($(window).scrollTop())
-	    if ($(window).scrollTop() > 280) {
+	    if ($(window).scrollTop() > 608) {
 	      $('nav').addClass('navbar-fixed');
 	    }
-	    if ($(window).scrollTop() < 281) {
+	    if ($(window).scrollTop() < 609) {
 	      $('nav').removeClass('navbar-fixed');
 	    }
 	  });
